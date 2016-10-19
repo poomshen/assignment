a560d46ba7321e2ead43ec5187ff20340c817875
이예지 <yegy314g@gmail.com>
2016-10-19 19:48:15 +0900

    2016/10/19과제 완료

--- 이예지/test_yj/src/ex03/Program.java
+++ 이예지/test_yj/src/ex03/Program.java
@@ -13,9 +13,10 @@ public class Program {
 		viewer.setRecorder(new Recorder());
 		*/
 		
+		ApplicationContext context = new GenericXmlApplicationContext("classpath:ex03/ApplicationContext.xml");
+		MonitorViewer viewer=context.getBean("monitorViewer",MonitorViewer.class);
+	
 		
-	    //System.out.println(viewer.getRecorder());
-		//viewer.RecorderPrint();	
 	}
 
 }
