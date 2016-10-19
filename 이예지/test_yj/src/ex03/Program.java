ff51af16f24bcd28fec003e7abcc934e83ab64a6
이성준 <poomshen@gmail.com>
2016-10-19 18:51:40 +0900

    숙제

---
+++ 이예지/test_yj/src/ex03/Program.java
@@ -0,0 +1,21 @@
+package ex03;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+//import org.springframework.beans.factory.annotation.AutowiredAnnotationBeanPostProcessor;
+public class Program {
+
+	public static void main(String[] args) {
+		
+		/*
+		MonitorViewer viewer = new MonitorViewer();
+		viewer.setRecorder(new Recorder());
+		*/
+		
+		
+	    //System.out.println(viewer.getRecorder());
+		//viewer.RecorderPrint();	
+	}
+
+}
