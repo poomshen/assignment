0e581f6a8a3f30a649433f9acb15b34ccccae81f
이성준 <poomshen@gmail.com>
2016-10-26 14:36:18 +0900

    과제

---
+++ 과제/SpringMVC_Basic06_WebSite_Annotation_jdbcTemplate/src/ncontroller/MembersController.java
@@ -0,0 +1,60 @@
+package ncontroller;
+
+import java.sql.SQLException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+import dao.MemberDao;
+import vo.Member;
+
+@Controller
+public class MembersController {
+	
+	private MemberDao memberDao;
+	@Autowired
+	public void setMemberDao(MemberDao memberDao) {
+		this.memberDao = memberDao;
+	}
+
+	@RequestMapping(value="joinus/join.htm",method=RequestMethod.GET)
+	public String insertMember(){
+		return "join.jsp";
+	}
+	
+	@RequestMapping(value="joinus/join.htm",method=RequestMethod.POST)
+	public String insertMember(Member member) throws ClassNotFoundException, SQLException{
+		System.out.println("여기 탄다");
+		int ck = memberDao.insert(member);
+		if(ck > 0){
+			System.out.println("회원가입 성공");
+		}
+		return "redirect:login.htm";
+	}
+	
+	
+	@RequestMapping(value="joinus/login.htm" , method=RequestMethod.GET)
+	public String longinMember(){
+		return "login.jsp";
+	}
+	
+	@RequestMapping(value="joinus/login.htm" , method=RequestMethod.POST)
+	public String longinMember(Member member) throws ClassNotFoundException, SQLException{
+		
+		Member members = memberDao.getMember(member.getUid());
+		if(members != null){
+			System.out.println(member.getUid());
+			System.out.println("로그인 성공");
+		}else{
+			System.out.println("로그인 실패");
+		}
+		return "redirect:login.htm";
+	}
+}
